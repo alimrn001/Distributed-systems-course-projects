2dc4ea11680069026f4655044ca9f039906dc11f
mehrani <mehraniali8002@gmail.com>
2024-04-22 18:32:56 +0330

    add functions for events CRUD (part 2 of assignment)

--- CA2/ticket-reservation/main.go
+++ CA2/ticket-reservation/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"time"
+	"ticket-reservation/utils"
+	"sync"
 )
 
 type Event struct {
 	ID					string
 	Name				string
-	Data				time.Time
+	Date				time.Time
 	TotalTickets		int
 	AvailableTickets 	int
 }
@@ -18,6 +20,49 @@ type Ticket struct {
 	EventID string
 }
 
+type TicketService struct {
+	events sync.Map
+}
+
+func NewTicketService() *TicketService {
+	return &TicketService{}
+}
+
+func (ts *TicketService) CreateEvent(name string, date time.Time, totalTickets int) (*Event, error) {
+	event := &Event{
+		ID:               utils.GenerateUUID(),
+		Name:             name,
+		Date:             date,
+		TotalTickets:     totalTickets,
+		AvailableTickets: totalTickets,
+	}
+
+	ts.events.Store(event.ID, event)
+	return event, nil
+}
+
+func (ts *TicketService) ListEvents() []*Event {
+	var events []*Event
+	ts.events.Range(func(key, value interface{}) bool {
+		event := value.(*Event)
+		events = append(events, event)
+		return true
+	})
+	return events
+}
+
+
 func main() {
-	fmt.Println("Hello world")
+	ticketService := NewTicketService()
+
+	// Creating new events
+	ticketService.CreateEvent("Concert", time.Now(), 100)
+	ticketService.CreateEvent("Conference", time.Now(), 200)
+
+	// Listing events
+	events := ticketService.ListEvents()
+	fmt.Println("Events:")
+	for _, event := range events {
+		fmt.Printf("ID: %s, Name: %s, Date: %s, Total Tickets: %d, Available Tickets: %d\n", event.ID, event.Name, event.Date.String(), event.TotalTickets, event.AvailableTickets)
+	}
 }
